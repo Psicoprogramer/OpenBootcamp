cda603406e51627cfef624036a74ea2d9bc81c39
Psicoprogramer <109642500+Psicoprogramer@users.noreply.github.com>
2022-10-14 19:10:41 -0500

    Create tarea.js

---
+++ tarea.js
@@ -0,0 +1,17 @@
+let name = "Brayan   "
+let lastName = "Cabrera"
+let student= name + " " + lastName
+
+console.log(student);
+
+let estudianteMayus = student.toUpperCase();
+
+console.log(estudianteMayus);
+let estudianteMinus = student.toLowerCase();
+console.log(estudianteMinus);
+
+let mumberC = console.log(student.length); 
+let primeraLetraName = console.log(name.charAt(0));
+let ultimaLetAp = console.log(lastName.charAt(6));
+let eliminarSpaces = console.log(student.trim());
+let ConsultarName = console.log(student.includes(name));
